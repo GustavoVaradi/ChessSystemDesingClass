3c75c5696870a5648d70baefb67aeb6dbca88907
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-04-19 14:13:48 -0300

    first class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("hello world!");
+		
+		var pos = new Position(3,5); 
+		
+		System.out.println(pos);	
+		
 	}
 }
