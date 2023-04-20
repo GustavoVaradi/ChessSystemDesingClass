0b95a78202421c90f4b7a737e11a5ebeed35cbed
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-04-20 16:30:32 -0300

    Handling exceptions and cleaning screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -13,18 +15,29 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			
-			UI.printBoard(chessMatch.getPieces());
-			
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(input);
-			
-			System.out.println();
-			System.out.print("Tareget: ");
-			ChessPosition target = UI.readChessPosition(input);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {
+				
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(input);
+				
+				System.out.println();
+				System.out.print("Tareget: ");
+				ChessPosition target = UI.readChessPosition(input);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+			} catch (ChessException erro) {
+				System.out.println(erro.getMessage());
+				input.nextLine();
+				
+			} catch (InputMismatchException erro) {
+				System.out.println(erro.getMessage());
+				input.nextLine();
+			}
 		}
 		
 	}
--- src/application/UI.java
+++ src/application/UI.java
@@ -29,18 +29,23 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-	
+
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner input) {
 		try {
-			
+
 			String s = input.nextLine();
 			char column = s.charAt(0);
-			// recortando a parte final da string "a1, b2, d8, etc."  
+			// recortando a parte final da string "a1, b2, d8, etc."
 			// e transformando em inteiro
 			int row = Integer.parseInt(s.substring(1));
-			
+
 			return new ChessPosition(column, row);
-			
+
 		} catch (RuntimeException error) {
 			throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
 		}
@@ -71,5 +76,5 @@ public class UI {
 		}
 		System.out.print(" ");
 	}
-	
+
 }
