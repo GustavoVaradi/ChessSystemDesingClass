9eb50d998e8e3dae14e3133c709ca4217c31762e
Gustavo Varadi <varadi.gustavo@outlook.com>
2023-04-24 16:33:12 -0300

    Check Logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import boardgame.Board;
 import boardgame.Piece;
@@ -14,16 +15,18 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	private boolean check;
 	
 	private List<Piece> piecesOnTheBoard;
 	private List<Piece> capturedPieces;
-
+	
 	public ChessMatch() {
 		board = new Board(8, 8);
 		turn = 1;
 		currentPlayer = Color.WHITE;
 		piecesOnTheBoard = new ArrayList<>();
 		capturedPieces = new ArrayList<>();
+		check = false;
 		initialSetUp();
 	}
 	
@@ -33,6 +36,10 @@ public class ChessMatch {
 	
 	public Color getCurrentPlayer() {
 		return currentPlayer;
+	}
+	
+	public boolean getCheck() {
+		return check;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -63,6 +70,13 @@ public class ChessMatch {
 		
 		Piece capturedPiece = makeMove(source, target);
 		
+		if (testCheck(currentPlayer)) {
+			undoMove(source, target, capturedPiece);
+			throw new ChessException("You can't put yourself in check!");
+		}
+		
+		check = (testCheck(opponent(currentPlayer))) ? true : false;
+		
 		nextTurn();
 		// DownCasting para ChessPiece.
 		// Como o método retorna um ChessPiece como diz na assinatura do método]
@@ -83,6 +97,19 @@ public class ChessMatch {
 		}
 		
 		return capturedPiece;
+	}
+	
+	private void undoMove(Position source, Position target, Piece capturedPiece) {
+		Piece piece = board.removePiece(target);
+		board.placePiece(piece, source);
+		
+		if (capturedPiece != null) {
+			board.placePiece(capturedPiece, target);
+			
+			capturedPieces.remove(capturedPiece);
+			piecesOnTheBoard.add(capturedPiece);
+			
+		}
 	}
 	
 	private void validateSourcePosition(Position position) {
@@ -112,6 +139,37 @@ public class ChessMatch {
 		turn ++;
 		//            // se o jogador atual FOI branco, então agora ele É preto caso contrário ele é o branco
 		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
+	
+	private Color opponent(Color color) {
+		
+		return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
+	
+	private ChessPiece king(Color color) {
+		List<Piece> listPieces = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+		
+		for (Piece piece : listPieces) {
+			if (piece instanceof King) {
+				return (ChessPiece) piece;
+			}
+		}
+		
+		throw new IllegalStateException("There is no " + color + " king on the board");
+	}
+	
+	private boolean testCheck(Color color) {
+		Position kingPosition = king(color).getChessPosition().toPosition();
+		List<Piece> opponentPieces = piecesOnTheBoard.stream().filter(x -> ((ChessPiece) x).getColor() == opponent(color)).collect(Collectors.toList());
+		
+		for (Piece piece : opponentPieces) {
+			boolean[][] matrix = piece.possibleMoves();
+			if (matrix[kingPosition.getRow()][kingPosition.getColumn()] == true) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
